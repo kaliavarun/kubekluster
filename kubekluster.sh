1153320ad27d1757ebfb3a4e41aadbf5a8c19d89
varun <arian.veron@gmail.com>
2021-05-23 15:43:46 +0200

    fixed issue with copying kubernetes file

--- kubekluster.sh
+++ kubekluster.sh
@@ -6,7 +6,7 @@ echo "Initilaizing a highly available Kubernetes cluster..."
 
 NODE_PASSWORD=$(yq e '.NODE_PASSWORD' config.yml)
 
-vagrant up &> vagrant.log
+#vagrant up &> vagrant.log
 
 MASTER_IP_RANGE=$(yq e '.MASTER_IP_RANGE' config.yml)
 
@@ -17,6 +17,8 @@ rm -R -f .tmp/
 
 rm -R -f ~/.kube
 mkdir ~/.kube
-sshpass -p $NODE_PASSWORD  root@${MASTER_IP_RANGE}1:/etc/kubernetes/admin.conf ~/.kube/config
+CMD="sshpass -p $NODE_PASSWORD scp  root@${MASTER_IP_RANGE}1:/etc/kubernetes/admin.conf ~/.kube/config"
+eval $CMD
+
 
 echo "Kubernetes cluster has been setup successfully!!"
